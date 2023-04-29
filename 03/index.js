63dc9464aed200f5b6244811a74f9654ad787e10
Gabriel Vargas <gabriel.vargas.coelho@gmail.com>
2023-04-29 16:34:02 -0300

    feature: adiciona resolução de o3

--- 03/index.js
+++ 03/index.js
@@ -1,5 +1,15 @@
 // Crie sua função aqui
-
+function biggestNumber(numbers) {
+  let biggest
+  numbers.forEach((number, index) => {
+    if (index === 0) {
+      biggest = number
+      return
+    }
+    if (number > biggest) biggest = number
+  })
+  return biggest
+}
 
 // Não altere as linhas abaixo
 console.log(biggestNumber([0, 1, 2, 3, 4, 5]))
