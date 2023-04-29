ab8af39cd60e51757d2ed84e88b4759655fa5382
Gabriel Vargas <gabriel.vargas.coelho@gmail.com>
2023-04-29 16:35:41 -0300

    feature: adiciona resolução de 04

--- 04/index.js
+++ 04/index.js
@@ -1,5 +1,12 @@
 // Crie sua função aqui
-
+function oneToNumber(number) {
+  const array = []
+  if (number <= 0) return array
+  for (let i = 1; i <= number; i++) {
+    array.push(i)
+  }
+  return array
+}
 
 // Não altere as linhas abaixo
 console.log(oneToNumber(10))
